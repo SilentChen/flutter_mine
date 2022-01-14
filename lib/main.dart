e986bf80f6227715ba34f7c2df8f0fad283f9113
fuck <fuckyou@now.com>
2022-01-14 17:13:36 +0800

    init

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mine/util/mineStyle.dart';
+
+void main() {
+  if(kReleaseMode) {
+    ErrorWidget.builder = (FlutterErrorDetails flutterErrorDetails) {
+      debugPrint(flutterErrorDetails.toString());
+      return Material(child: Center(child: Text("Error, contact developer\nemail: 609451243@qq.com", textAlign: TextAlign.center)));
+    };
+  }
+  runApp(MineApp());
+}
+
+class MineApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "MineApp",
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        hintColor: Colors.white,
+        accentColor: Colors.white,
+        primaryColor: MineColor.orange,
+        primaryColorBrightness: Brightness.dark,
+        scaffoldBackgroundColor: MineColor.back1,
+        dialogBackgroundColor: MineColor.back2,
+        accentColorBrightness: Brightness.light,
+        textTheme: TextTheme(bodyText1: MineTextStyle.normal),
+      ),
+      home: HomePage(),
+    )
+  }
+}
---
+++ lib/page/homePage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
+  
+}
